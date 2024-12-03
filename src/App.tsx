754a7f96df3434742225f75edd7b1a3e94c44f05
Anton Zabelin <azabelin@t1-consulting.ru>
2024-12-03 19:42:14 +0300

    Two blocks

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,35 @@ import { BoxList } from './BoxList';
 import { BoxType } from './BoxType';
 
 const initialBoxes: BoxType[] = [
-  { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 3, name: 'Item 3' }
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
 ];
 
 function App() {
-  const [boxes, setBoxes] = useState<BoxType[]>(initialBoxes);
-  const [selectedBoxes1, setSelectedBoxes1] = useState<BoxType[]>([]);
-  const [selectedBoxes2, setSelectedBoxes2] = useState<BoxType[]>([]);
+    const [boxes, setBoxes] = useState<BoxType[]>(initialBoxes);
+    const [selectedBoxes, setSelectedBoxes] = useState<BoxType[]>([]);
 
-  const moveBox = (num: number, box: BoxType)=> {
-    switch (num) {
-      case 1: setSelectedBoxes1([...selectedBoxes1, box]); break;
-      case 2: setSelectedBoxes2([...selectedBoxes2, box]); break;
-    }
-    setBoxes(boxes.filter((b) => b.id !== box.id));
-  };
+    const moveBox = (box: BoxType) => {
+        setSelectedBoxes([...selectedBoxes, box]);
+        setBoxes(boxes.filter((b) => b.id !== box.id));
+    };
 
-  const onDrop = (num:number, item: BoxType) => {
-    const box = boxes.find((b) => b.name === item.name);
-    if (box) {
-      moveBox(num, box);
-    }
-  };
+    const onDrop = (item: BoxType) => {
+        const box = boxes.find((b) => b.name === item.name);
+        if (box) {
+            moveBox(box);
+        }
+    };
 
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <div style={{ display: 'flex' }}>
-          <BoxList title="Available Items" boxes={boxes} />
-          <BoxList title="Selected Items" boxes={selectedBoxes1} onDrop={(item: BoxType) => onDrop(1, item)} />
-          <BoxList title="Selected Items" boxes={selectedBoxes2} onDrop={(item: BoxType) => onDrop(2, item)} />
-      </div>
-    </DndProvider>
-  );
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div style={{ display: 'flex' }}>
+                <BoxList title='Available Items' boxes={boxes} />
+                <BoxList title='Selected Items' boxes={selectedBoxes} onDrop={onDrop} />
+            </div>
+        </DndProvider>
+    );
 }
 
 export default App;
--- src/BoxList.tsx
+++ src/BoxList.tsx
@@ -1,36 +1,36 @@
-import React, { FC } from "react";
-import { Box } from "./Box";
-import update from "immutability-helper";
-import { NativeTypes } from "react-dnd-html5-backend";
-import { useDrop } from "react-dnd";
-import { ItemTypes } from "./ItemTypes";
+import React, { FC } from 'react';
+import { Box } from './Box';
+import update from 'immutability-helper';
+import { NativeTypes } from 'react-dnd-html5-backend';
+import { useDrop } from 'react-dnd';
+import { ItemTypes } from './ItemTypes';
 
 interface Props {
-  title: string;
-  boxes: { id: number; name: string }[];
-  onDrop?: (item: any) => void;
+    title: string;
+    boxes: { id: number; name: string }[];
+    onDrop?: (item: any) => void;
 }
 
 export const BoxList: FC<Props> = ({ title, boxes, onDrop }) => {
-  const [, drop] = useDrop({
-    accept: [NativeTypes.URL, ItemTypes.BOX],
-    drop(item: any, monitor) {
-      if (onDrop) {
-        onDrop(item);
-      }
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
-  });
+    const [, drop] = useDrop({
+        accept: [NativeTypes.URL, ItemTypes.BOX],
+        drop(item: any, monitor) {
+            if (onDrop) {
+                onDrop(item);
+            }
+        },
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+    });
 
-  return (
-    <div ref={drop}>
-      <h3>{title}</h3>
-      {boxes.map((box) => (
-        <Box key={box.id} name={box.name} />
-      ))}
-    </div>
-  );
+    return (
+        <div ref={drop} style={{ border: '1px solid black', padding: '1rem', margin: '1rem' }}>
+            <h3>{title}</h3>
+            {boxes.map((box) => (
+                <Box key={box.id} name={box.name} />
+            ))}
+        </div>
+    );
 };
